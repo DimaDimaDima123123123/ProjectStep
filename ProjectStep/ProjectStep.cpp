ab4285b15c287708c0a0492de2c7d42077838f06
DimaDimaDima123123123 <dmitriyshevel11@gmail.com>
2024-09-10 17:26:50 +0300

    класс студент

--- ProjectStep/ProjectStep.cpp
+++ ProjectStep/ProjectStep.cpp
@@ -1,71 +1,88 @@
 ﻿#include <iostream>
+#include <Windows.h>
+
 using namespace std;
 
-class Fraction
+class Student
 {
 private:
-    int x;
-    int y;
+    char name[50];        
+    char birthdate[20];    
+    char phone[20];      
+    char city[30];        
+    char country[30];      
+    char education[50];   
+    char classnumber[10];  
+    
 public:
-    Fraction(int valueX, int valueY)
+    Student(const char* Name, const char* Birthdate, const char* Phone, const char* City, const char* Country, const char* Education, const char* Classnumber)
     {
-        x = valueX;
-        y = valueY;
+        strcpy_s(name, Name);
+        strcpy_s(birthdate, Birthdate);
+        strcpy_s(phone, Phone);
+        strcpy_s(city, City);
+        strcpy_s(country, Country);
+        strcpy_s(education, Education);
+        strcpy_s(classnumber, Classnumber);
     }
-    void SetX(int valueX)
+
+    void Input()
     {
-        cout << "Введите X: ";
-        cin >> valueX;
-        x = valueX;
+        cout << "Введите ФИО: ";
+        cin.getline(name, 40);
+        cout << "Введите дату рождения: ";
+        cin.getline(birthdate, 10);
+        cout << "Введите телефон: ";
+        cin.getline(phone, 10);
+        cout << "Введите город: ";
+        cin.getline(city, 15);
+        cout << "Введите страну: ";
+        cin.getline(country, 15);
+        cout << "Введите название учебного заведения: ";
+        cin.getline(education, 20);
+        cout << "Введите номер группы: ";
+        cin.getline(classnumber, 5);
     }
-    void SetY(int valueY)
+
+    void Print() const
     {
-        cout << "Введите Y: ";
-        cin >> valueY;
-        y = valueY;
+        cout << "ФИО: " << name << endl;
+        cout << "Дата рождения: " << birthdate << endl;
+        cout << "Телефон: " << phone << endl;
+        cout << "Город: " << city << endl;
+        cout << "Страна: " << country << endl;
+        cout << "Учебное заведение: " << education << endl;
+        cout << "Номер группы: " << classnumber << endl;
     }
-    void Calc(int choice)
+
+    void SetName(const char* Name) { strcpy_s(name, Name); }
+    const char* GetName() const { return name; }
+
+    void SetPhone(const char* Phone)
     {
-        cout << "Выберите желаемую операцию:\n";
-        cout << "1. Сложение\n";
-        cout << "2. Вычитание\n";
-        cout << "3. Умножение\n";
-        cout << "4. Деление\n";
-        cin >> choice;
-        switch (choice)
-        {
-        case 1:
-            cout << "X" << "+" << "Y"<< "=" << x + y;
-            break;
-        case 2:
-            cout << "X" << "-" << "Y"<< "=" << x - y;
-            break;
-        case 3:
-            cout << "X" << "*" << "Y" << "=" << x * y;
-            break;
-        case 4:
-            cout << "X" << "/" << "Y" << "=" << x / y;
-            break;
-        default:
-            cout << "Неизвестная операция.";
-            break;
-        }
+        strcpy_s(phone, Phone);
     }
-    void Print()
+    const char* GetPhone() const
     {
-        cout << "X = " << x << "\t Y = " << y << endl;
+        return phone;
     }
+
+    ~Student()
+    {
+
+    }
+
 };
-
-
 
 int main()
 {
     setlocale(LC_ALL, "ru");
 
-    Fraction a(0, 0);
-    a.SetX(0);
-    a.SetY(0);
-    a.Print();
-    a.Calc(0);
+    Student student("Зубенко Михаил Петрович", "08.03.1969", "+3805633465", "Троицкое", "Украина", "ПТУ", "КН-1337");
+    student.Print();
+    cout << "\nВведите новые данные студента:\n";
+    student.Input();
+    student.Print();
+
+    return 0;
 }
